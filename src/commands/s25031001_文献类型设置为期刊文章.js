d0a8b26e312e83c3032a2d6e0288a0d55692bb99
panhaoyu <panhaoyu.china@outlook.com>
2025-03-15 11:29:34 +0800

    add: s25031001_文献类型设置为期刊文章.js

--- src/commands/s25031001_文献类型设置为期刊文章.js
+++ src/commands/s25031001_文献类型设置为期刊文章.js
@@ -10,6 +10,15 @@ await Zotero.DB.executeTransaction(async () => {
   for (const item of items) {
     if (!targetTypes.has(item.itemType)) continue
 
+    // 删除当前条目的PDF附件
+    const attachmentIDs = item.getAttachments()
+    for (const id of attachmentIDs) {
+      const attachment = Zotero.Items.get(id)
+      if (attachment.attachmentContentType === "application/pdf") {
+        await attachment.erase()
+      }
+    }
+
     // 转换条目类型并清理标题格式
     item.setType(journalArticleID)
     const originalTitle = item.getField("title")
