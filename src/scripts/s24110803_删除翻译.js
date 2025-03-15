72c5931ebd7432d9e590fdd11f81945fad2931d1
panhaoyu <panhaoyu.china@outlook.com>
2025-03-15 11:01:26 +0800

    add: s25031501_笔记样式.css

--- src/scripts/s24110803_删除翻译.js
+++ src/scripts/s24110803_删除翻译.js
@@ -1,25 +1,19 @@
-/**
- * 如果 DOI 存在且格式正确，则删除 URL 字段
- *
- * @param {Array} items - 选中的文献条目列表
- */
-async function processItems(items) {
+(async () => {
+  // eslint-disable-next-line no-undef
+  const items = Zotero.getActiveZoteroPane().getSelectedItems()
 
   if (!items || items.length === 0) {
     return;
   }
 
   for (let item of items) {
-    const extra = item.getField("extra") || "";  // 获取 'extra' 字段
+    const extra = item.getField("extra") || "";
     const newExtra = extra
-      .split("\n")                               // 将内容按行分割
-      .filter(line => !line.trim().startsWith("titleTranslation:"))  // 删除包含 'titleTranslation:' 的行
-      .join("\n");                               // 重新组合成字符串
+      .split("\n")
+      .filter(line => !line.trim().startsWith("titleTranslation:"))
+      .join("\n");
 
-    item.setField("extra", newExtra);            // 设置新的 'extra' 字段内容
-    await item.saveTx();                         // 保存更改
+    item.setField("extra", newExtra);
+    await item.saveTx();
   }
-}
-
-// 执行处理选中文献的函数
-await processItems(items);
+})().then()
